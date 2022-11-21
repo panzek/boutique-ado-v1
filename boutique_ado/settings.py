37dc374875dd3c28694692d9e9e04861c7a98dc0
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-11-21 14:37:49 +0000

    Add home app and tempplates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -83,7 +84,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
