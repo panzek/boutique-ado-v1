f27e2b0ea4ab0b0b34917564824d843ef4fd68e0
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-11-23 16:13:14 +0000

    Add products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL) # a foreign key to the category model
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+# a foreign key to the category model. We'll allow this to be null in the database and blank in forms 
+# and if a category is deleted we'll set any products that use it to have null for this field rather than deleting the product.
+
+# it's worth noting what we're requiring here; each product requires a name, a description, and a price. But everything else is optional.
