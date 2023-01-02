58dd443ea8953957000485383e05dd286d867d26
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-02 16:22:38 +0000

    Securing the views

--- profiles/views.py
+++ profiles/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
+from django.contrib.auth.decorators import login_required
 
 from checkout.models import Order
 
-
+@login_required
 def profile(request):
     """ Display the user's profile. """
 
